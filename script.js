c35fa3f5d3700ea07d7b3b8bb2bd3f1b6e5b984c
Atlanq093 <laura.lewtak@gmail.com>
2026-03-20 22:16:22 +0100

    bugs repair

--- script.js
+++ script.js
@@ -10,15 +10,27 @@ const winTriesEl = document.getElementById('win-tries')
 const restartBtn = document.getElementById('restartBtn')
 const diffBtns = document.querySelectorAll('.diffBtn')
 const images = [
-     "imagines/img1.avif",
-    "imagines/img3.jpg",
-     "imagines/img9.png",
-    "imagines/img4.jpg",
-    "imagines/img5.avif",
-    "imagines/img6.webp",
-    "imagines/img7.webp",
-    "imagines/img8.jpg",
-];
+    "imagines/img1.avif",
+    "imagines/img2.png",
+    "imagines/img3.png",
+    "imagines/img4.avif",
+    "imagines/img5.png",
+    "imagines/img6.png",
+    "imagines/img7.png",
+    "imagines/img8.png",
+    "imagines/img9.png",
+    "imagines/img10.png",
+    "imagines/img11.png",
+    "imagines/img12.png",
+    "imagines/img13.png",
+    "imagines/img14.png",
+    "imagines/img15.png",
+    "imagines/img16.png",
+    "imagines/img17.png",
+    "imagines/img18.png",
+    "imagines/img19.png",
+    "imagines/img20.png",
+]
 
 // === ZMIENNE GRY ===
 let flippedCards = []
@@ -41,12 +53,29 @@ diffBtns.forEach(btn => {
 
 //Karty 
 function startGame(size){
+     console.log('size:', size)
+    console.log('pairsNeeded:', (size * size) / 2)
     gameGrid.innerHTML = "";
     flippedCards = [];
     tries = 0;
     triesEl.innerHTML = `Próby: 0`;
+
+    gameGrid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    const pairsNeeded = (size * size) / 2
+    if(size === 4){
+    gameGrid.style.setProperty('--card-width', '150px')
+    gameGrid.style.setProperty('--card-height', '160px')
+} else if(size === 6){
+    gameGrid.style.setProperty('--card-width', '120px')
+    gameGrid.style.setProperty('--card-height', '120px')
+} else {
+    gameGrid.style.setProperty('--card-width', '130px')
+    gameGrid.style.setProperty('--card-height', '140px')
+}
+    const selectedImages = images.slice(0, pairsNeeded)
+    const cards = [...selectedImages, ...selectedImages]
     
-     const cards = [...images, ...images] //shuffle method 
+    //shuffle method 
     cards.sort(() => 0.5 - Math.random())
     //Tworzenie kart
     cards.forEach(value => {
@@ -91,7 +120,7 @@ function startGame(size){
 
     //TIMER 
     clearInterval(timerInterval)
-    seconds= 60
+    seconds = size === 4 ? 60 : size === 6 ? 120 : 180;
     timerEl.innerHTML = `Czas: ${seconds}s`
     timerInterval = setInterval(() => {
         seconds--
@@ -143,10 +172,13 @@ function checkWin() {
     winMessage.classList.remove('hidden')
     }
 }
-
+//restart button
 restartBtn.addEventListener('click', () => {
     winMessage.classList.add('hidden')
-    gameGrid.classList.add('hidden')
+    gameGrid.style.visibility = 'visible'
+    gameGrid.style.opacity = '1'
+    gameGrid.style.display = 'none'
+    infoBar.style.visibility = 'visible'
     infoBar.style.display = 'none'
     startScreen.classList.remove('hidden')
     flippedCards = []
@@ -154,11 +186,13 @@ restartBtn.addEventListener('click', () => {
     seconds = 0
     clearInterval(timerInterval)
 })
-
-//game-over restart
+//game over
 document.getElementById('restartBtnGameOver').addEventListener('click', () => {
     document.getElementById('game-over').classList.add('hidden')
     gameGrid.style.visibility = 'visible'
+    gameGrid.style.opacity = '1'
+    gameGrid.style.display = 'none'
+    infoBar.style.visibility = 'visible'
     infoBar.style.display = 'none'
     startScreen.classList.remove('hidden')
     flippedCards = []
